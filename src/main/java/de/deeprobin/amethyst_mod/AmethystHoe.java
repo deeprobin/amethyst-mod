02fab8680a49accded5d9307dc13840ae2da7c1d
Robin Lindner <robin@deeprobin.de>
2020-08-30 14:13:07 +0200

    Initial commit

---
+++ src/main/java/de/deeprobin/amethyst_mod/AmethystHoe.java
@@ -0,0 +1,10 @@
+package de.deeprobin.amethyst_mod;
+
+import net.minecraft.item.HoeItem;
+import net.minecraft.item.ToolMaterial;
+
+final class AmethystHoe extends HoeItem {
+    public AmethystHoe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
